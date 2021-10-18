28030fa09bc025a86f618066553b7d8dc77142ec
CasaNovack <92743291+CasaNovack@users.noreply.github.com>
2021-10-18 18:09:28 +0200

    Add files via upload

---
+++ Zad2.cpp
@@ -0,0 +1,32 @@
+﻿// Zad2.cpp :2. Napisać program, który sprawdza, czy podana liczba całkowita, wczytana z klawiatury 
+//jest parzysta, czy nieparzysta. (operator %).
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "podaj liczbe calkowita  ";
+	cin >> a;
+	if (a % 2 == 0)
+	{
+		cout << "liczba " << a << " jest parzysta";
+	}
+	else
+	{
+		cout << "liczba " << a << " jest nieparzysta";
+	}
+	return 0;
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
---
+++ Zad5.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a, b, c ;
+	int dzialanie;
+	cout << "podaj 2 liczby";
+	cin >> a >> b;
+	cout << "wybierz działanie:\n1.dodawanie\n2odejmowanie\n3.mnozenie\n4.dzielenie\n";
+	cin >> dzialanie;
+	switch (dzialanie)
+	{
+	case 1: cout << "a + b = " << a + b;
+	case 2: cout << "a - b = " << a - b;
+	case 3: cout << "a * b = " << a * b;
+	case 4:
+		if (b == 0) cout << "nie dziel pajacu przez 0";
+		else cout << "a / b = " << a / b;
+	}
+			return 0;
+	
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
